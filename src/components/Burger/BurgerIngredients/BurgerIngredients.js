99c984bd7bc2773bb4391aacfa739db5b994bdc6
waqasmalik1010 <waqasmalikbsse@gmail.com>
2019-12-12 14:01:44 +0500

    Starting the Burger Component

--- src/components/Burger/BurgerIngredients/BurgerIngredients.js
+++ src/components/Burger/BurgerIngredients/BurgerIngredients.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import classes from 'BurgerIngredients.module.css'
+import classes from './BurgerIngredients.module.css'
 
 const burgerIngredients = (props) => {
     let ingredients = null;
