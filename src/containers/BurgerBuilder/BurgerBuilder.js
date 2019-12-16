fb7baa094ac7abe1f8e406ffba371fea185c3951
waqasmalik1010 <waqasmalikbsse@gmail.com>
2019-12-16 18:04:52 +0500

    Changing the Folder Structure

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import Burger from '../../components/Burger/Burger'
-import Aux from '../../hoc/Aux'
+import Aux from '../../hoc/Aux/Aux'
 import BuildControls from '../../components/Burger/BuildControls/BuildControls'
 import Modal from '../../components/Modal/Modal'
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary'
--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -1,5 +1,5 @@
 import React, {Component} from  'react'
-import Aux from '../../hoc/Aux'
+import Aux from '../Aux/Aux'
 import classes from './Layout.module.css'
 import Toolbar from '../../components/Navigation/Toolbar/Toolbar'
 import SideDrawer from '../../components/Navigation/SideDrawer/SideDrawer'
