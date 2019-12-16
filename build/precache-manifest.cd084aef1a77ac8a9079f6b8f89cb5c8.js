bcba5545dc1cdf79a365879cc0190304b255fa37
waqasmalik1010 <waqasmalikbsse@gmail.com>
2019-12-16 18:54:41 +0500

    Added Build Files

---
+++ build/precache-manifest.cd084aef1a77ac8a9079f6b8f89cb5c8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b73891834b4cc7dcfee85baa664b7894",
+    "url": "/index.html"
+  },
+  {
+    "revision": "492b043b3bd8c914688d",
+    "url": "/static/css/main.7d8d64f8.chunk.css"
+  },
+  {
+    "revision": "050895c6bb2a004cd4d6",
+    "url": "/static/js/2.ff1b4ff0.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.ff1b4ff0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "492b043b3bd8c914688d",
+    "url": "/static/js/main.f976e7b1.chunk.js"
+  },
+  {
+    "revision": "2c4b2280a128f2ea552c",
+    "url": "/static/js/runtime-main.dd5c6174.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/static/media/burger-logo.b8503d26.png"
+  }
+]);
