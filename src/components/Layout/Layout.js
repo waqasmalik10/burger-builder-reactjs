d123059d0fede6af8a9459307bda9eadc03da147
waqasmalik1010 <waqasmalikbsse@gmail.com>
2019-12-16 17:13:13 +0500

    Reusing the Backdrop

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,17 +1,29 @@
-import React from  'react'
+import React, {Component} from  'react'
 import Aux from '../../hoc/Aux'
 import classes from './Layout.module.css'
 import Toolbar from '../../components/Navigation/Toolbar/Toolbar'
 import SideDrawer from '../../components/Navigation/SideDrawer/SideDrawer'
 
-const Layout = (props) => (
-    <Aux>
-        <Toolbar/>
-        <SideDrawer/>
-        <main className={classes.Content}>
-            {props.children}
-        </main>
-    </Aux>
-)
+class Layout extends Component {
+
+    state = {
+        showSideDrawer : true
+    }
+    sideDrawerClosedHandler = () => {
+        this.setState({showSideDrawer : false});
+    }
+    render() {
+        return (
+            <Aux>
+                <Toolbar/>
+                <SideDrawer show={this.state.showSideDrawer} clicked={this.sideDrawerClosedHandler}/>
+                <main className={classes.Content}>
+                    {this.props.children}
+                </main>
+            </Aux>
+        )
+
+    }
+}
 
 export default Layout;  
--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -2,15 +2,22 @@ import React from 'react'
 import Logo from '../../Logo/Logo'
 import NavigationItems from '../../Navigation/NavigationItems/NavigationItems'
 import classes from './SideDrawer.module.css'
+import BackDrop from '../../UI/Backdrop/Backdrop'
+import Aux from '../../../hoc/Aux'
 
 const sideDrawer = (props) => {
+    let attachedClasses = [classes.SideDrawer, classes.Close];
+    if(props.show) attachedClasses = [classes.SideDrawer, classes.Open];
     return (
-        <div className={classes.SideDrawer}>
-            <Logo height="8%"/>
-            <nav>
-                <NavigationItems />
-            </nav>
-        </div>
+        <Aux>
+            <BackDrop show={props.show} clicked={props.clicked}/>
+            <div className={attachedClasses.join(" ")}>
+                <Logo height="8%"/>
+                <nav>
+                    <NavigationItems />
+                </nav>
+            </div>
+        </Aux>
     );
 }
 
