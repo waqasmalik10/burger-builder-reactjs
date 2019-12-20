77018bff4d766441e10033d0861440e29f9294e8
waqasmalik1010 <waqasmalikbsse@gmail.com>
2019-12-20 15:01:31 +0500

    Adding an Orders page

---
+++ src/containers/Orders/Orders.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Order from '../../components/Order/Order'
+
+
+class Orders extends Component {
+    render() {
+        return (
+            <div>
+                <Order/>
+                <Order/>
+            </div>
+        )
+    }
+}
+
+export default Orders;
