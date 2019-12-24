a8e1eba70f9177947d038d1ebd7a131d2ef8c465
waqasmalik1010 <waqasmalikbsse@gmail.com>
2019-12-24 18:38:40 +0500

    Installing redux and react-redux

--- src/index.js
+++ src/index.js
@@ -4,10 +4,17 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter } from 'react-router-dom';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+import reducer from './store/reducer'
+
+const store = createStore(reducer);
 
 const app = (
     <BrowserRouter>
-        <App/>
+        <Provider store={store}>
+            <App/>
+        </Provider>
     </BrowserRouter>
 )
 
