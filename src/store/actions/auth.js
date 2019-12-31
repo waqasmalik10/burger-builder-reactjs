01ea76692863fe5f5cdafaa5c0b69dbe55b63fb0
waqasmalik1010 <waqasmalikbsse@gmail.com>
2019-12-31 19:43:17 +0500

    Getting a token from the Backend

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,4 +1,5 @@
-import * as actionTypes from './actionTypes'
+import * as actionTypes from './actionTypes';
+import axios from 'axios';
 
 export const authStart = () => {
     return {
@@ -23,5 +24,19 @@ export const authFail = (error) => {
 export const auth = (email, password) => {
     return dispatch => {
         dispatch(authStart());
+        const authData = {
+            email: email, 
+            password: password,
+            returnSecureToken: true
+        }
+        axios.post('https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyBtGHiJ0e63A1_PT5lTwSXh5spb1SuiQRQ', authData)
+        .then(response => {
+            console.log(response    );
+            dispatch(authSuccess(response.data))
+        })
+        .catch(err => {
+            console.log(err);
+            dispatch(authFail(err))
+        })
     }
 }
