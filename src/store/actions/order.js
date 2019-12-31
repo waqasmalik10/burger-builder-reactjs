1badada6c49a698284499c7c0580a619d1da42dd
waqasmalik1010 <waqasmalikbsse@gmail.com>
2019-12-31 13:18:29 +0500

    Fetching Orders (via Redux)

--- src/store/actions/order.js
+++ src/store/actions/order.js
@@ -40,3 +40,44 @@ export const purchaseInit = () => {
         type: actionTypes.PURCHASE_INIT
     }
 }
+
+export const fetchOrderStart = () => {
+    return {
+        type: actionTypes.FETCH_ORDERS_START
+    }
+}
+
+export const fetchOrderSuccess = orders => {
+    return {
+        type: actionTypes.FETCH_ORDERS_SUCCESS,
+        orders: orders
+    }
+}
+
+export const fetchOrderFailed = error => {
+    return {
+        type: actionTypes.FETCH_ORDERS_FAILED,
+        error: error
+    }
+}
+
+export const fetchOrders = () => {
+    return dispatch => {
+        dispatch(fetchOrderStart());
+        axios.get('/orders.json')
+        .then(res => {
+            console.log(res.data);
+            const fetchedOrders = [];
+            for(let key in res.data) {
+                fetchedOrders.push({
+                    ...res.data[key],
+                    id: key
+                });
+            }
+            dispatch(fetchOrderSuccess(fetchedOrders));
+        })
+        .catch(err => {
+            dispatch(fetchOrderFailed(err));
+        });
+    }
+}
