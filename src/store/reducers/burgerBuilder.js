53cb19486587cd8ff445ba62b1a385752955a7d0
waqasmalik1010 <waqasmalikbsse@gmail.com>
2019-12-31 12:33:14 +0500

    Resetting the Price after Purchases

--- src/store/reducers/burgerBuilder.js
+++ src/store/reducers/burgerBuilder.js
@@ -55,7 +55,8 @@ const reducer = (state = initialState, action) => {
                     cheese: action.ingredients.cheese,
                     meat: action.ingredients.meat
                 },
-                error: false
+                error: false,
+                totalPrice: 4
             }
         case actionTypes.FETCH_INGREDIENTS_FAILED:
             return {
