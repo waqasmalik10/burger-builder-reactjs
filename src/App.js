722dc711142eb78b6ef67646caabc083c18bed5e
waqasmalik1010 <waqasmalikbsse@gmail.com>
2020-01-01 18:09:32 +0500

    Guarding Routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Route, Switch, withRouter} from 'react-router-dom'
+import {Route, Switch, withRouter, Redirect} from 'react-router-dom'
 import {connect} from 'react-redux'
 import Layout from './hoc/Layout/Layout'
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder'
@@ -16,15 +16,27 @@ class App extends Component {
   }
 
   render() {
-    return (
-      <Layout>
+    let routes = (
+      <Switch>
+          <Route path="/auth" component={Auth} />
+          <Route path="/" exact component={BurgerBuilder} />
+          <Redirect to="/" />
+        </Switch>
+    );
+    if(this.props.isAuthenticated) {
+      routes = (
         <Switch>
           <Route path="/checkout" component={Checkout} />
           <Route path="/orders" component={Orders} />
-          <Route path="/auth" component={Auth} />
           <Route path="/logout" component={Logout} />
           <Route path="/" exact component={BurgerBuilder} />
-          </Switch>
+          <Redirect to="/" />
+        </Switch>
+      );
+    }
+    return (
+      <Layout>
+        {routes}
       </Layout>
     );
   }
@@ -33,7 +45,7 @@ class App extends Component {
 
 const mapStateToProps = state => {
   return {
-
+    isAuthenticated: state.auth.idToken !== null
   }
 }
 
