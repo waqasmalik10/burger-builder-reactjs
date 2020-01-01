7fb71fe10551a0878493703efbdc34101a7fd979
waqasmalik1010 <waqasmalikbsse@gmail.com>
2020-01-01 17:42:47 +0500

    Persistent Auth State with localStorage

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {Route, Switch} from 'react-router-dom'
+import {connect} from 'react-redux'
 import Layout from './hoc/Layout/Layout'
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder'
 import Checkout from './containers/Checkout/Checkout'
 import Orders from './containers/Orders/Orders'
 import Auth from './containers/Auth/Auth'
 import Logout from './containers/Auth/Logout/Logout'
+import * as actions from './store/actions/index'
 
-function App() {
-  return (
-    <Layout>
-      <Switch>
-        <Route path="/checkout" component={Checkout} />
-        <Route path="/orders" component={Orders} />
-        <Route path="/auth" component={Auth} />
-        <Route path="/logout" component={Logout} />
-        <Route path="/" exact component={BurgerBuilder} />
-        </Switch>
-    </Layout>
-  );
+class App extends Component {
+
+  componentDidMount() {
+    this.props.onTryAutoSignup()
+  }
+
+  render() {
+    return (
+      <Layout>
+        <Switch>
+          <Route path="/checkout" component={Checkout} />
+          <Route path="/orders" component={Orders} />
+          <Route path="/auth" component={Auth} />
+          <Route path="/logout" component={Logout} />
+          <Route path="/" exact component={BurgerBuilder} />
+          </Switch>
+      </Layout>
+    );
+  }
+  
 }
 
-export default App;
+const mapStateToProps = state => {
+  return {
+
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    onTryAutoSignup: () => dispatch(actions.authCheckState())
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -23,16 +23,19 @@ export const authFail = (error) => {
 }
 
 export const logout = () => {
+    localStorage.removeItem('token');
+    localStorage.removeItem('expirationDate');
+    localStorage.removeItem('userId');
     return {
         type: actionTypes.AUTH_LOGOUT
     }
 }
 
-export const checkAuthTimeout = expirationTime => {
+export const checkAuthTimeout = expirationDate => {
     return dispatch => {
         setTimeout(() => {
             dispatch(logout());
-        }, expirationTime * 1000)
+        }, expirationDate * 1000)
     }
 }
 
@@ -50,7 +53,11 @@ export const auth = (email, password, isSignup) => {
         }
         axios.post(url, authData)
         .then(response => {
-            console.log(response    );
+            console.log(response);
+            const expirationDate = new Date(new Date().getTime() + (response.data.expiresIn * 1000));
+            localStorage.setItem('token', response.data.idToken);
+            localStorage.setItem('expirationDate', expirationDate);
+            localStorage.setItem('userId', response.data.localId);
             dispatch(authSuccess(response.data.idToken, response.data.localId));
             dispatch(checkAuthTimeout(response.data.expiresIn))
         })
@@ -65,5 +72,24 @@ export const setAuthRedirectPath = (path) => {
     return {
         type: actionTypes.SET_AUTH_REDIRECT_PATH,
         path: path
+    }
+}
+
+export const authCheckState = () => {
+    return dispatch => {
+        const token = localStorage.getItem('token');
+        if(!token) {
+            dispatch(logout());
+        }  else {
+            const expirationDate = new Date(localStorage.getItem('expirationDate'));
+            if(expirationDate > new Date()) {
+                dispatch(authSuccess(token, localStorage.getItem('userId')))
+                dispatch(checkAuthTimeout(expirationDate.getSeconds() - new Date().getSeconds()))
+            } else {
+                dispatch(logout());
+            }
+
+            
+        }
     }
 }
